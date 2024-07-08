24e8bac01d75637aafa68efd73b2e3cd5f43dd1a
Karine Meireles <karinelameireles@hotmail.com>
2024-07-08 12:39:03 -0300

    meu terceiro commit

---
+++ cypress/e2e/Cadastro.cy.js
@@ -0,0 +1,87 @@
+/// <reference types="cypress"/>
+
+describe('US-012-Funcionalidade: Cadastro de membros', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  });
+
+  it('Deve fazer o cadastro de campos obrigatórios', () => {
+    var email = `karine${Date.now()}@teste.com`
+  cy.preencherCadastro('Karine' , 'Lima' , email , '11925256042' , 'Teste@25896')
+    cy.get('#signup-response').should('contain' , 'Cadastro realizado com sucesso!')
+  });
+
+
+  it('Deve validar mensagem de erro com o campo nome inválido', () => {
+    cy.preencherCadastro('Karine10' , 'Lima' , 'karine123@teste.com' , '11925256042' , 'Teste@25896')
+    cy.get('#signup-response').should('contain' , 'Nome deve conter apenas caracteres alfabéticos, acentuados e espaços')
+  });
+
+
+  
+
+  it('Deve validar mensagem e erro E-mail deve ser um email válido', () => {
+    cy.preencherCadastro('Karine' , 'Lima' , 'antonio.com' , '11925256042' , 'Teste@25896')
+    cy.get('#signup-response').should('contain' , 'E-mail deve ser um email válido')
+  });
+
+
+
+  it('Deve validar a mensagem de caracteres minimos para senha segura', () => {
+    var email = `karine${Date.now()}@teste.com`
+    cy.preencherCadastro('Karine' , 'Lima' , email , '11925256042' , '5896')
+    cy.get('#signup-response').should('contain' , 'Senha deve ter pelo menos 8 caracteres, incluir uma letra maiúscula, um número e um caractere especial (!@#$&*)')
+  });
+
+
+
+  it('Deve validar mensagem e erro Este email já está cadastrado.', () => {
+    cy.preencherCadastro('Karine' , 'Lima' , "karine123@teste.com" , '11925256042' , '@Tau5896')
+    cy.get('#signup-response').should('contain' , 'Este email já está cadastrado.')
+  });
+
+
+
+  it('Deve validar mensagem e erro no campo Nome não pode estar vazio', () => {
+    var email = `karine${Date.now()}@teste.com`
+    cy.preencherCadastro( '' , 'Lima' , email , '11925256042' , 'Teste@25896')
+    cy.get('#signup-response').should('contain' , 'Nome não pode estar vazio')
+  });
+
+
+  it('Deve validar mensagem e erro no campo Sobrenome não pode estar vazio', () => {
+    var email = `karine${Date.now()}@teste.com`
+    cy.preencherCadastro( 'Karine' , '' , email , '11925256042' , 'Teste@25896')
+    cy.get('#signup-response').should('contain' , 'Sobrenome não pode estar vazio')
+  });
+
+
+  it('Deve validar a mensagem e erro Senha não pode estar vazia', () => {
+    var email = `karine${Date.now()}@teste.com`
+    cy.preencherCadastro( 'Karine' , 'Antonio' , email , '11925256042' , '')
+    cy.get('#signup-response').should('contain' , 'Senha não pode estar vazia')
+  });
+
+
+  it('Deve validar mensagem e erro Este E-mail não pode estar vazio', () => {
+    var email = `karine${Date.now()}@teste.com`
+    cy.preencherCadastro( 'Karine' , 'Antonio' , "" , '11925256042' , '!Tag45678')
+    cy.get('#signup-response').should('contain' , 'E-mail não pode estar vazio')
+  });
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ cypress/e2e/busca.cy.js
@@ -0,0 +1,40 @@
+/// <reference types="cypress"/>
+
+describe('US-001-Funcionalidade: Busca de filmes', () => {
+   beforeEach(() => {
+    cy.visit('/')
+   });
+
+   it('Deve buscar filmes com sucesso', () => {
+    cy.get('#search-input').type('The godfather')
+    cy.get('#search-button').click()
+    cy.get('#results-section').should('contain' , 'The Godfather')
+   });
+
+   it('Deve buscar filmes com sucesso de uma lista', () => {
+    cy.fixture('filmes').then((filmes) =>{
+    cy.get('#search-input').type(filmes[6].titulo)
+    cy.get('#search-button').click()
+    cy.get('#results-section').should('contain' , filmes[6].titulo)     
+    })
+   });
+
+   it('Busca de filme sem resultado', () => {
+    cy.fixture('filmes').then((filmes) =>{
+    cy.get('#search-input').type(filmes[1].titulo)
+    cy.get('#search-button').click()
+    cy.get('#results-section').should('contain' , 'Filme não encontrado.')     
+    })
+   });
+   
+   it('Limpar a busca de filmes', () => {
+    cy.fixture('filmes').then((filmes) =>{
+    cy.get('#search-input').type(filmes[3].titulo)
+    cy.get('#search-button').click()
+    cy.get('#results-section').should('contain' , filmes[3].titulo)
+    cy.get('#clear-button').click()
+    cy.get('#results-section').clear
+    })
+   });
+    
+});
