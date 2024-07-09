91e4c4c6e318c29c9394b4c51e8923f7a5bb8562
Karine Meireles <karinelameireles@hotmail.com>
2024-07-09 02:07:47 -0300

    ci-cd

--- cypress/e2e/Cadastro.cy.js
+++ cypress/e2e/Cadastro.cy.js
@@ -5,6 +5,12 @@ describe('US-012-Funcionalidade: Cadastro de membros', () => {
     cy.visit('/')
   });
 
+  afterEach(() => {
+    cy.screenshot()
+ });
+
+
+ 
   it('Deve fazer o cadastro de campos obrigatórios', () => {
     var email = `karine${Date.now()}@teste.com`
   cy.preencherCadastro('Karine' , 'Lima' , email , '11925256042' , 'Teste@25896')
